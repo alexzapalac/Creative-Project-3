51dbc4fd928268c77326aca579e2b52515708079
alexzapalac <alexzapalac@gmail.com>
2021-03-19 22:01:30 -0600

    more items

--- src/mock-data.js
+++ src/mock-data.js
@@ -89,6 +89,216 @@ let mock = [{
     otherName: "Penstemon dolius M.E. Jones ex Pennell var. duchesnensis N.H. Holmgren",
     family: "Scrophulariaceae",
     image: "sweet-alyssum.jpg"
-  }]
+  }, {
+  id: 16,
+  name: "Entosthodon Moss",
+  otherName: "Entosthodon fascicularis (Hedw.) Müll. Hal.",
+  family: "Funariaceae",
+  image: "peonies.jpg"
+}, {
+  id: 17,
+  name: "Starflower Brodiaea",
+  otherName: "Brodiaea stellaris S. Watson",
+  family: "Liliaceae",
+  image: "spirit.jpg"
+}, {
+  id: 18,
+  name: "Small-leaf Bentgrass",
+  otherName: "Agrostis microphylla Steud.",
+  family: "Poaceae",
+  image: "impatiens.jpg"
+}, {
+  id: 19,
+  name: "Woolly Burdock",
+  otherName: "Arctium tomentosum Mill.",
+  family: "Asteraceae",
+  image: "bloodgood.jpg"
+}, {
+  id: 20,
+  name: "Stebbins' Desertparsley",
+  otherName: "Lomatium stebbinsii Schlessman & Constance",
+  family: "Apiaceae",
+  image: "maple-blaze.jpg"
+}, {
+  id: 21,
+  name: "Longleaf Groundcherry",
+  otherName: "Physalis longifolia Nutt. var. subglabrata (Mack. & Bush) Cronquist",
+  family: "Solanaceae",
+  image: "yarrow.jpg"
+}, {
+  id: 22,
+  name: "Gordon's Bladderpod",
+  otherName: "Lesquerella gordonii (A. Gray) S. Watson",
+  family: "Brassicaceae",
+  image: "willow.jpg"
+}, {
+  id: 23,
+  name: "Wetherill's Cryptantha",
+  otherName: "Cryptantha wetherillii (Eastw.) Payson",
+  family: "Boraginaceae",
+  image: "hawthorn.jpg"
+}, {
+  id: 24,
+  name: "Dense Blazing Star",
+  otherName: "Liatris spicata (L.) Willd.",
+  family: "Asteraceae",
+  image: "silver.jpg"
+}, {
+  id: 25,
+  name: "Lakeshore Sedge",
+  otherName: "Carex lenticularis Michx. var. impressa (L.H. Bailey) L.A. Standl.",
+  family: "Cyperaceae",
+  image: "royal.jpg"
+}, {
+  id: 26,
+  name: "Arizona Yellowhood",
+  otherName: "Nissolia wislizeni (A. Gray) A. Gray",
+  family: "Fabaceae",
+  image: "rodgers.jpg"
+}, {
+  id: 27,
+  name: "Yarrow",
+  otherName: "Achillea L.",
+  family: "Asteraceae",
+  image: "plantain.jpg"
+}, {
+  id: 28,
+  name: "Fiveangled Dodder",
+  otherName: "Cuscuta pentagona Engelm. var. pentagona",
+  family: "Cuscutaceae",
+  image: "hydrangea.jpg"
+}, {
+  id: 29,
+  name: "Pink Redstem",
+  otherName: "Ammannia latifolia L.",
+  family: "Lythraceae",
+  image: "bloodroot.jpg"
+}, {
+  id: 30,
+  name: "Hydrocotyle",
+  otherName: "Hydrocotyle L.",
+  family: "Apiaceae",
+  image: "poplar.jpg"
+}, {
+  id: 31,
+  name: "Mexican Blazingstar",
+  otherName: "Mentzelia mexicana H.J. Thomp. & Zavort.",
+  family: "Loasaceae",
+  image: "oak.jpg"
+}, {
+  id: 32,
+  name: "Santa Catalina Monkeyflower",
+  otherName: "Mimulus traskiae A.L. Grant",
+  family: "Scrophulariaceae",
+  image: "miss-kim.jpg"
+}, {
+  id: 33,
+  name: "Bractless Blazingstar",
+  otherName: "Mentzelia nuda (Pursh) Torr. & A. Gray var. nuda",
+  family: "Loasaceae",
+  image: "maple-tree.jpg"
+}, {
+  id: 34,
+  name: "Rim Lichen",
+  otherName: "Lecanora caesiosulphurea Vain.",
+  family: "Lecanoraceae",
+  image: "maple.jpg"
+}, {
+  id: 35,
+  name: "False Boneset",
+  otherName: "Brickellia eupatorioides (L.) Shinners var. eupatorioides",
+  family: "Asteraceae",
+  image: "inkberry.jpg"
+}, {
+  id: 36,
+  name: "Louisiana Catchfly",
+  otherName: "Silene subciliata B.L. Rob.",
+  family: "Caryophyllaceae",
+  image: "hollyhock.jpg"
+}, {
+  id: 37,
+  name: "Gentian",
+  otherName: "Gentiana L.",
+  family: "Gentianaceae",
+  image: "gold.jpg"
+}, {
+  id: 38,
+  name: "Gila River Dodder",
+  otherName: "Cuscuta applanata Engelm.",
+  family: "Cuscutaceae",
+  image: "daylily.jpg"
+}, {
+  id: 39,
+  name: "Hayfield Tarweed",
+  otherName: "Hemizonia congesta DC. ssp. clevelandii (Greene) Babc. & H.M. Hall",
+  family: "Asteraceae",
+  image: "cypress.jpg"
+}, {
+  id: 40,
+  name: "Entireleaf Nitrogen Moss",
+  otherName: "Tetraplodon mnioides (Hedw.) Bruch & Schimp.",
+  family: "Splachnaceae",
+  image: "arbor.jpg"
+}, {
+  id: 41,
+  name: "Largeleaf Phyllostegia",
+  otherName: "Phyllostegia macrophylla (Gaudich.) Benth.",
+  family: "Lamiaceae",
+  image: "snowball.jpg"
+}, {
+  id: 42,
+  name: "California Ayenia",
+  otherName: "Ayenia compacta Rose",
+  family: "Sterculiaceae",
+  image: "sweet-woodruff.jpg"
+}, {
+  id: 43,
+  name: "Hawthorn",
+  otherName: "Crataegus ×lettermanii Sarg. (pro sp.)",
+  family: "Rosaceae",
+  image: "sharon.jpg"
+}, {
+  id: 44,
+  name: "Arid Tansyaster",
+  otherName: "Machaeranthera arida B.L. Turner & Horne",
+  family: "Asteraceae",
+  image: "nellie.jpg"
+}, {
+  id: 45,
+  name: "Pygmy Pussytoes",
+  otherName: "Antennaria monocephala DC.",
+  family: "Asteraceae",
+  image: "mondo.jpg"
+}, {
+  id: 46,
+  name: "Western Yampah",
+  otherName: "Perideridia erythrorhiza (Piper) T.I. Chuang & Constance",
+  family: "Apiaceae",
+  image: "ruby.jpg"
+}, {
+  id: 47,
+  name: "Mountain Stopper",
+  otherName: "Eugenia reinwardtiana (Blume) DC.",
+  family: "Myrtaceae",
+  image: "kerria.jpg"
+}, {
+  id: 48,
+  name: "Little Nipple Cactus",
+  otherName: "Mammillaria meiacantha Engelm.",
+  family: "Cactaceae",
+  image: "flag-northern.jpg"
+}, {
+  id: 49,
+  name: "Parasite Syrrhopodon Moss",
+  otherName: "Syrrhopodon parasiticus (Brid.) Besch.",
+  family: "Calymperaceae",
+  image: "caradonna.jpg"
+}, {
+  id: 50,
+  name: "Heartleaf Bittercress",
+  otherName: "Cardamine cordifolia A. Gray var. cordifolia",
+  family: "Brassicaceae",
+  image: "butterfly.jpg"
+}]
 
   export default mock;
